01c6f83e3a5d03dd9dbcb968ba7c0af8737f3ff6
Илья <anons.rru@mail.ru>
2020-12-27 15:01:59 +0300

    Добавьте файлы проекта.

---
+++ Practice/Practice.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "ru");
+
+    vector<char> myVector;
+
+
+
+
+
+
+    string name = "myfile.txt";
+    ifstream fin;
+
+    fin.open(name);
+
+    if (!fin.is_open())
+    {
+        cout << "Ошибка открытия файла!" << endl;
+
+    }
+    else
+    {
+        cout << "Файл открыт" << endl;
+    }
+    char ch;
+    int numb;
+    int numb2;
+    numb = 1;
+    numb2 = 1;
+    while (fin.get(ch))
+    {
+        myVector.push_back(ch);
+        numb++;
+
+        cout << ch;
+        if (numb == 12)
+        {
+            cout << "\n";
+            numb = 0;
+            numb2++;
+        }
+        if (numb == numb2)
+        {
+            cout << "*";
+        }
+    }
+}
